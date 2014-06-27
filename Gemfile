fd335590e6f71abc7fa1c94555916033f558e219
Christopher Mehmed <chrismehmed@gmail.com>
2014-06-27 16:50:05 -0400

    view profile pages

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'sinatra'
 gem 'activerecord'
 gem 'sinatra-activerecord'
 gem 'rake'
+gem 'rack-flash3'
 
 group :development do
 	gem 'sqlite3'
--- app.rb
+++ app.rb
@@ -2,11 +2,18 @@
 
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'bundler/setup'
+require 'sinatra/base'
+require 'rack-flash'
 
-configure(:development){ set :database, "sqlite3:project_database.sqlite3"}
+enable :sessions
+use Rack::Flash, :sweep => true
 set :sessions, true
 
+configure(:development){ set :database, "sqlite3:project_database.sqlite3"}
+
 require './models'
+
 
 def current_user
 	if session[:user_id]
@@ -16,21 +23,24 @@ end
 
 helpers { alias_method :current_user, :current_user}
 
+
+#GET ROUTES
+
+
 get '/' do
 	erb :Welcome
 end
 
 
-get '/user_not_found' do
-	erb :user_not_found
+get '/new_user' do
+	erb :new_user
 end
 
 
-get '/success' do
+get '/profile' do
 	@user = current_user
-	helpers { alias_method :current_user, :current_user}
 
-	erb :success
+	erb :profile
 end
 
 
@@ -39,20 +49,34 @@ get '/login_failed' do
 end
 
 
+get '/all_users' do
+	@users = User.all
+
+	erb :all_users
+end
+
+
+#POST ROUTES
+
+
 post '/sign_in' do 
 	@user = User.where(email: params[:email]).first
 	if !@user
-		redirect "/user_not_found"
-	elsif @user.password == params[:password] 
-		redirect "/success"   
+		redirect "/new_user"
+	elsif @user.password == params[:password]
+		session[:user_id] = @user.id
+		redirect "/profile"
 	else 
-		redirect "/login_failed"
+		redirect "/"
+		flash[:notice] = "That password doesn't match this email.  Try again!"
 	end
 end
 
 post '/sign_up' do
 	puts params[:user]
 	@user = User.create(params[:user])
+	redirect "/"
+	flash[:notice] = "Cool. Let's make sure you can sign in."
 end
 
 
